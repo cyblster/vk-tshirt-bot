af7fee849649d88f66bfd2b310ed6cd5025ea60f
cyblster <kiuspensky@gmail.com>
2023-03-06 19:00:53 +0500

    init

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,35 @@
+import requests
 from flask import Flask
 
+import config
 
-app = Flask(__name__)
+
+class MyApp(Flask):
+    def __init__(self, import_name: str):
+        self.confirm_string = config.API_CONFIRM_STRING
+
+        self.api_url = 'https://api.vk.com/method/{method}'
+        self.api_token = config.API_TOKEN
+        self.api_version = config.API_VERSION
+
+        self.group_id = None
+        self.post_id = None
+
+        super().__init__(import_name)
+
+    def is_ready(self) -> bool:
+        if None not in (self.group_id, self.post_id):
+            return True
+        return False
+
+    def reply_to_comment(self, group_id: int, post_id: int, comment_id: int, message: str) -> None:
+        requests.get(self.api_url.format(method='wall.createComment'), params={
+            'access_token': self.api_token, 'owner_id': -group_id, 'post_id': post_id,
+            'reply_to_comment': comment_id, 'message': message, 'v': self.api_version
+        })
+
+
+app = MyApp(__name__)
 
 
 from . import handlers
--- app/handlers.py
+++ app/handlers.py
@@ -10,4 +10,23 @@ def root():
     if event['type'] == 'confirmation':
         return config.API_CONFIRM_STRING, 200
 
-    print(event)
+    if event['type'] == 'wall_post_new':
+        if app.__group_id is None:
+            app.group_id = event['group_id']
+
+        if app.post_id is None:
+            app.post_id = event['id']
+
+        return '', 200
+
+    if event['type'] == 'wall_reply_new':
+        if not app.is_ready():
+            return {'error': 'Need a new post on the wall before the bot works'}, 409
+
+        if 'attachments' not in event['object']:
+            app.reply_to_comment(app.group_id, app.post_id, event['object']['id'], 'Ответочка')
+        if event['object']['attachments']['type'] != 'photo':
+            return 'Unsupported media type', 415
+
+        embed_url = event['object']['attachments']['photo']['sizes'][-1]['url']
+
