84c7c16f5b19788820230ba9727a27cbd4c9a1a1
cyblster <kiuspensky@gmail.com>
2023-03-06 19:17:23 +0500

    init

--- app/handlers.py
+++ app/handlers.py
@@ -15,17 +15,19 @@ def root():
     if event['type'] == 'wall_post_new':
         if app.post_id is None:
             app.post_id = event['object']['id']
-            return {'error': False, 'message': 'Post has been successfully created'}, 200
-        return {'error': True, 'message': 'Post already created'}, 200
+
+        return '', 200
 
     if event['type'] == 'wall_reply_new':
         if not app.is_ready():
-            return {'error': True, 'message': 'Need a new post on the wall before the bot works'}, 200
+            return '', 200
 
         if 'attachments' not in event['object']:
             app.reply_to_comment(app.group_id, app.post_id, event['object']['id'], 'Ответочка')
+            return '', 200
+
         if event['object']['attachments']['type'] != 'photo':
-            return {'error': True, 'message': 'Unsupported media type'}, 200
+            return '', 200
 
         embed_url = event['object']['attachments']['photo']['sizes'][-1]['url']
 
